674ef55779a950d4392bae268ddbb8e0187e4192
Henrique Nince <75384272+henriquenince@users.noreply.github.com>
2026-05-20 10:07:43 -0300

    Update cluster_pon.R

--- 5._Clusterização/cluster_pon.R
+++ 5._Clusterização/cluster_pon.R
@@ -13,8 +13,6 @@ library(cluster)
 library(NbClust)
 library(FSA)
 
-setwd("D:/Henrique Nince/OneDrive/Área de Trabalho/UnB/TCC/Análises/6ª Tentativa/4. Tratamento final, Análise Fatorial e Scores")
-
 scores_pon <- read_excel("scores_pontas.xlsx", sheet = 1)
 
 # Selecionando o numero de clusters
